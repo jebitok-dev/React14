045e2486eadf7d64696b3357a21dcc6dd51fc174
jebitok-dev <sharonkosgei4@gmail.com>
2020-09-07 21:46:58 +0300

    set-up react project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+   render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>GROMONI</h1>
+        </header>
+      </div>
+    );
+  }  
+}
+  
+export default App;
