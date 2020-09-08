803d2eecf6509afc947bd55ce533b1d785d1b8a5
jebitok-dev <sharonkosgei4@gmail.com>
2020-09-08 22:51:07 +0300

    setup sass and created header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './header/header.component';
 
 class App extends React.Component {
    render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>GROMONI</h1>
-        </header>
+        <header />
       </div>
     );
   }  
---
+++ src/header/header.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.styles.scss';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-title">
+                GROMONI
+            </div>
+        </div>
+    );
+} 
+
+export default Header;
