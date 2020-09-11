7bee9c434dddaa769b4659b0c58f6d906cc55674
jebitok-dev <sharonkosgei4@gmail.com>
2020-09-11 19:03:31 +0300

    testimonial sec

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './header/header.component';
+import Header from './testimonial/testimonial';
 
 
 class App extends Component  {
@@ -8,6 +8,11 @@ class App extends Component  {
     return (
       <div className="App">
         <h1 className="header">Gromoni</h1>
+        <h3>How it Works</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing consequat sem vel fermentum nisl et.
+ Erat a ac blandit neque amet pharetra ornare non nibh. Quam ultrices integer neque cras cum quam egestas faucibus. 
+Non blandit at in sem ipsum est est.</p>
+      //Testimonial
         <header />
       </div>
     );
