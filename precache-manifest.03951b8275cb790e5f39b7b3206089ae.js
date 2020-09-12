f70c409c658a12bf8edf1337183b4b667ce97138
jebitok-dev <sharonkosgei4@gmail.com>
2020-09-12 14:20:48 +0300

    Updates

---
+++ precache-manifest.03951b8275cb790e5f39b7b3206089ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36aa03ec9e87754290b32de7254cbc52",
+    "url": "/jebitok-dev/React14/index.html"
+  },
+  {
+    "revision": "d22dbb0675e4c67a93a9",
+    "url": "/jebitok-dev/React14/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "cc98d409c6e9337a1b68",
+    "url": "/jebitok-dev/React14/static/css/main.a6aeab8f.chunk.css"
+  },
+  {
+    "revision": "d22dbb0675e4c67a93a9",
+    "url": "/jebitok-dev/React14/static/js/2.339a7bb0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/jebitok-dev/React14/static/js/2.339a7bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc98d409c6e9337a1b68",
+    "url": "/jebitok-dev/React14/static/js/main.6b1d5017.chunk.js"
+  },
+  {
+    "revision": "af7600ecf06e301d483d",
+    "url": "/jebitok-dev/React14/static/js/runtime-main.ce2e4d12.js"
+  }
+]);
